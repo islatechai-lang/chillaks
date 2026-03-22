3717ddbc10d2d8b1efa269f05254b597375349cc
shankiz <princederder44@gmail.com>
2026-03-23 03:43:42 +0800

    Fix formatting and ignore eslint on build

--- app/api/token/route.ts
+++ app/api/token/route.ts
@@ -18,7 +18,6 @@ const LIVEKIT_URL = process.env.LIVEKIT_URL;
 export const revalidate = 0;
 
 export async function POST(req: Request) {
- 
   try {
     if (LIVEKIT_URL === undefined) {
       throw new Error('LIVEKIT_URL is not defined');
